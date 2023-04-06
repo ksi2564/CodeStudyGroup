c8a32511a1dafffa49d0a2ebe610e8dcb54a0fe6
Inys <kimsungin5@gmail.com>
2023-04-06 23:47:36 +0900

    김성인 : 백준 3109 빵집

---
+++ src/seongin/week04/P3109_빵집.java
@@ -0,0 +1,53 @@
+package seongin.week04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P3109_빵집 {
+    static int R, C;
+    static char[][] map;
+    static int cnt = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        map = new char[R][C];
+
+        for (int i = 0; i < R; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+
+        for (int i = 0; i < R; i++) {
+            if (check(i, 0)) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+
+    static boolean check(int x, int y) {
+        map[x][y] = '-';
+
+        if (y == C - 1) {
+            return true;
+        }
+
+        if (x > 0 && map[x - 1][y + 1] == '.') // 오른쪽 위
+            if (check(x - 1, y + 1))
+                return true;
+        if (map[x][y + 1] == '.') // 오른쪽
+            if (check(x, y + 1))
+                return true;
+        if (x + 1 < R && map[x + 1][y + 1] == '.') // 오른쪽 아래
+            if (check(x + 1, y + 1))
+                return true;
+        return false;
+    }
+}
